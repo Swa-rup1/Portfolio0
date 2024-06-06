7ca0bbd6b437a973d348d262764f5370f6fafc93
Swarup Bamjan <bamjanswarup666@gmail.com>
2024-06-06 15:52:47 +0545

    Added Changes

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ document.addEventListener('scroll', () => {
 });
 
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const img = document.getElementById('bg-pic');
+
+  img.addEventListener('click', () => {
+    document.body.classList.toggle('white-bg');
+
+  });
+});
+
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileNav = document.getElementById('mobile-nav');
+
+hamburgerMenu.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+});
+
